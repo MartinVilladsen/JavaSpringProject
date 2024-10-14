02535203595d63f750783b0edb86a20fd735ea8d
MartinVilladsen <martin.k.villadsen@gmail.com>
2024-10-14 15:25:38 +0200

    Java Spring

---
+++ src/main/java/dev/martinvilladsen/liverpool/Application.java
@@ -0,0 +1,19 @@
+package dev.martinvilladsen.liverpool;
+
+import foo.bar.WelcomeMessage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+		var welcomeMessage = new WelcomeMessage();
+		System.out.println(welcomeMessage.getWelcomeMessage());
+	}
+
+
+
+}
